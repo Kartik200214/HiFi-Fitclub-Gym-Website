9e3eee973e7e6a47a573b90481a476edc85cdb62
Kartik -KK <109861395+Kartik200214@users.noreply.github.com>
2023-06-24 17:00:47 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Footer.css'
+import Github from "../../assets/github.png"
+import Instagram from "../../assets/instagram.png"
+import LinkedIn from "../../assets/linkedin.png"
+import Logo from "../../assets/logo.png"
+import { Link } from 'react-scroll'
+
+
+const Footer = () => {
+  return (
+    <div className="Footer-container">
+        <hr/>
+        <div className="footer">
+            <div className="social-links">
+
+                <img src={Github} alt="" />
+                <img src={Instagram} alt="" />
+                <img src={LinkedIn} alt="" />
+            </div>
+            <div className="logo-f">
+             <img src={Logo} alt="" />
+             <div className='name'>© 2023 Kartik Kolhe</div>
+            </div> 
+        </div> 
+        <div className="blur blur-f-1"></div>
+        <div className="blur blur-f-2"></div>
+    </div>
+  )
+}
+
+export default Footer
